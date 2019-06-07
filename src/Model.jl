4f552cdfbd3899aaf0c7dcb3406cc22c3a6e7e41
silventino <silventino.dev@gmail.com>
2019-06-07 08:37:26 -0300

    Variáveis, função e várias constraints (ainda faltam algumas, tipo que autores não podem apresentar trabalhos no mesmo horario)

--- src/Model.jl
+++ src/Model.jl
@@ -66,8 +66,71 @@ function createModel(n_themes, n_authors, n_presentations, n_sessions, presentat
     end
 
 
+    
+    model = Model(with_optimizer(Gurobi.Optimizer))
 
+    @variable(model, 1 >= presentations_session[1:n_presentations, 1:n_sessions] >= 0, Int)
+    @variable(model, 1 >= presentations_are_in_same_session[1:n_presentations, 1:n_presentations] >= 0, Int)
+
+    for i in presentations
+        # CADA APRESENTAÇÃO TEM QUE ESTAR EM EXATAMENTE UMA SESSÃO
+        @constraint(model, sum(presentations_session[i,se] for se in sessions) == 1)
+        for j in presentations
+            for s in sessions
+                # SE DUAS SESSÕES ESTÃO NO MESMO HORÁRIO, presentations_are_in_same_session[i,j] = 1
+                @constraints(model, 
+                    begin
+                        presentations_are_in_same_session[i,j] <= presentations_session[i,s]
+                        presentations_are_in_same_session[i,j] <= presentations_session[j,s]
+                        presentations_are_in_same_session[i,j] >= presentations_session[i,s] + presentations_session[j,s] - 1
+                    end
+                )
+            end
+        end
+    end
+
+
+    # LIMITE TEMA X HORÁRIO
+    for t in themes
+        for s in keys(schedules_sessions)
+            @constraints(model, 
+                begin
+                    sum(sum(presentations_session[p, session_i] * presentations_themes[p, t] for session_i in schedules_sessions[s])  for p in presentations) <= 10
+                end
+            )
+        end
+    end
+
+    # LIMITE TEMA X DIA
+    for t in themes
+        for d in keys(dates_schedules)
+
+            @constraints(model, 
+                begin
+                    sum(
+                        sum(
+                            sum( 
+                                presentations_session[p, s] * presentations_themes[p,t] for p in presentations
+                            )  for s in schedules_sessions[schedule_i]
+                        ) for schedule_i in dates_schedules[d]) <= 10
+                end
+            )
+        end
+    end
+
+    for s in sessions
+        @constraints(model,
+            begin
+                sum(presentations_session[p, s] for p in presentations) <= max_capacity[s] 
+                sum(presentations_session[p, s] for p in presentations) >= min_capacity[s] 
+            end
+        )
+    end
+
+
+
+    print(model)
     println(schedules_sessions)
     println(dates_schedules)
-    println(presentations_authors)
+    # println(presentations_authors)
 end
