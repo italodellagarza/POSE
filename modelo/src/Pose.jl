3a8af6ed6201ddc2b260fcf9ea7006aca412a3a5
thuzax <arthurhscruz@gmail.com>
2019-07-02 17:02:23 -0300

    Faltou esses arquivos no commit anterior

--- modelo/src/Pose.jl
+++ modelo/src/Pose.jl
@@ -12,7 +12,7 @@ module Pose
 
     function __init__()
 
-        if size(ARGS)[1] < 3
+        if size(ARGS)[1] < 4
             println("É necessário passar um arquivo de entrada, além dos limites de temas por horario e dia")
             println("Exemplo de entrada: ")
             println("julia src/Pose.jl <arquivo de teste> <capacidade de horario> <capacidade por dia>")
@@ -22,39 +22,40 @@ module Pose
 
         # nThemes, nAuthors, nPresentations, nSessions, presentations, sessions = reader("./datafiles/problema1.txt")
         nThemes, nAuthors, nPresentations, nSessions, presentations, sessions = reader(ARGS[1])
-        schedule_capacity = parse(Int, ARGS[2])
-        date_capacity = parse(Int, ARGS[3])
+        output_file_name = ARGS[2]
+        schedule_capacity = parse(Int, ARGS[3])
+        date_capacity = parse(Int, ARGS[4])
         
-        println("Numero de temas = ", nThemes)
-        println("Numero de autores = ", nAuthors)
-        println("Numero de trabalhos = ", nPresentations)
-        println()
+        # println("Numero de temas = ", nThemes)
+        # println("Numero de autores = ", nAuthors)
+        # println("Numero de trabalhos = ", nPresentations)
+        # println()
 
-        for presentation in presentations
-            println("Trabalho: ", presentation.id)
-            println("Numero de temas ", presentation.nThemes)
-            println("Temas: ", presentation.themes)
-            println("Numero de Autores: ", presentation.nAuthors)
-            println("Autores: ", presentation.authors)
-            println()
-        end
-        println()
-        for session in sessions
-            println("Sessao: ", session.id)
-            println("Capacidade: ", session.capacity)
-            println("Tempo de apresentacao: ", session.presentationTime)
-            println("Dia e hora da apresentacao: ", session.schedule)
-            println("Dia da apresentacao: ", session.date)
-            println()
-        end
-        println()
-        println()
-        println()
-        println()
-        println()
-        println()
+        # for presentation in presentations
+        #     println("Trabalho: ", presentation.id)
+        #     println("Numero de temas ", presentation.nThemes)
+        #     println("Temas: ", presentation.themes)
+        #     println("Numero de Autores: ", presentation.nAuthors)
+        #     println("Autores: ", presentation.authors)
+        #     println()
+        # end
+        # println()
+        # for session in sessions
+        #     println("Sessao: ", session.id)
+        #     println("Capacidade: ", session.capacity)
+        #     println("Tempo de apresentacao: ", session.presentationTime)
+        #     println("Dia e hora da apresentacao: ", session.schedule)
+        #     println("Dia da apresentacao: ", session.date)
+        #     println()
+        # end
+        # println()
+        # println()
+        # println()
+        # println()
+        # println()
+        # println()
 
-        createModel(nThemes, nAuthors, nPresentations, nSessions, presentations, sessions, schedule_capacity, date_capacity)
+        createModel(nThemes, nAuthors, nPresentations, nSessions, presentations, sessions, schedule_capacity, date_capacity, output_file_name)
 
     end
 end
