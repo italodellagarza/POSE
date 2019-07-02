fac549477816698043b2a35768e25912fab6170d
joaopedroteo <jpteodoro2010@gmail.com>
2019-07-01 21:42:03 -0300

    heuristica

--- heuristica/src/Pose.jl
+++ heuristica/src/Pose.jl
@@ -19,30 +19,45 @@ module Pose
 
         # nThemes, nAuthors, nPresentations, nSessions, presentations, sessions = reader("./datafiles/problema1.txt")
         nThemes, nAuthors, nPresentations, nSessions, presentations, sessions = reader(ARGS[1])
-        println("Numero de temas = ", nThemes)
-        println("Numero de autores = ", nAuthors)
-        println("Numero de trabalhos = ", nPresentations)
-        println()
+        schedule_capacity = parse(Int, ARGS[2])
+        date_capacity = parse(Int, ARGS[3])
 
-        for presentation in presentations
-            println("Trabalho: ", presentation.id)
-            # println("Numero de temas ", presentation.nThemes)
-            println("Temas: ", presentation.themes)
-            # println("Numero de Autores: ", presentation.nAuthors)
-            println("Autores: ", presentation.authors)
-            println()
-        end
-        println()
-        for session in sessions
-            println("Sessao: ", session.id)
-            println("Capacidade: ", session.capacity)
-            # println("Tempo de apresentacao: ", session.presentationTime)
-            println("Dia e hora da apresentacao: ", session.schedule)
-            # println("Dia da apresentacao: ", session.date)
-            println()
-        end
-        println()
+        # println("Numero de temas = ", nThemes)
+        # println("Numero de autores = ", nAuthors)
+        # println("Numero de trabalhos = ", nPresentations)
+        # println()
 
-        heuristic(nThemes, nAuthors, nPresentations, nSessions, presentations, sessions)
+        # for presentation in presentations
+        #     println("Trabalho: ", presentation.id)
+        #     # println("Numero de temas ", presentation.nThemes)
+        #     println("Temas: ", presentation.themes)
+        #     # println("Numero de Autores: ", presentation.nAuthors)
+        #     println("Autores: ", presentation.authors)
+        #     println()
+        # end
+        # println()
+        # for session in sessions
+        #     println("Sessao: ", session.id)
+        #     println("Capacidade: ", session.capacity)
+        #     # println("Tempo de apresentacao: ", session.presentationTime)
+        #     println("Dia e hora da apresentacao: ", session.schedule)
+        #     # println("Dia da apresentacao: ", session.date)
+        #     println()
+        # end
+        # println()
+
+        # Numero de iterações da heuristica
+        n_iteration = 1
+        function_value, output, time_heuristic, percentage_of_errors = heuristic(nThemes, nAuthors, nPresentations, nSessions,
+                                                                                presentations, sessions, schedule_capacity, 
+                                                                                date_capacity, n_iteration)
+     
+        open("./results/temp.txt", "w") do f 
+            write(f, output)
+        end
+            
+        println("Taxa de erro: ", percentage_of_errors)
+        println("Time: ", time_heuristic)
+        println("Best funtion value: ", function_value)
     end
 end
--- heuristica/src/heuristic.jl
+++ heuristica/src/heuristic.jl
@@ -50,7 +50,7 @@ function is_possible_select(presentation, presentations_struct, session, session
         end
     end
 
-    #verifica se o autor já não está no horário
+    # verifica se o autor já não está no horário
     for session_ in sessions_struct
         if (session.schedule == session_.schedule)
             for presentation_id in session_.presentations
@@ -73,7 +73,7 @@ function is_possible_select(presentation, presentations_struct, session, session
 end
 
 
-function greedy_heuristic(n_themes, n_authors, n_presentations, n_sessions, presentations_struct, sessions_struct, presentations_similarity)    
+function greedy_heuristic(n_themes, n_authors, n_presentations, n_sessions, presentations_struct, sessions_struct, presentations_similarity, schedules_capacity, dates_capacity)    
     # Sessoes definidas como vetor de indices
     sessions = collect(1:n_sessions)
     # Apresentacoes definidas como vetor de indices
@@ -83,19 +83,34 @@ function greedy_heuristic(n_themes, n_authors, n_presentations, n_sessions, pres
     # Autores definidos como vetor de indices
     authors = collect(1:n_authors)
 
+    x = 0
+    presentations_ids = []
+    while (x < n_sessions)
+        presentations_ids = unique(append!(presentations_ids, trunc(Int, (rand()*100000) % n_presentations) + 1))
+        x, = size(presentations_ids)
+    end
+        
+    for i in collect(1:n_sessions)
+        # presentation_id = trunc(Int, presentations_first[i] * n_presentations) + 1
+        presentation_id = presentations_ids[i]
+
+        pf = presentations_struct[presentation_id]
+        new_presentation = Presentation(pf.id, pf.nThemes,
+                pf.nAuthors, pf.themes,
+                pf.authors, i)
+
+        presentations_struct[new_presentation.id] = new_presentation
+        append!(sessions_struct[i].presentations, presentation_id)
+    end
     
-    # Capacidade de cada horario
-    schedules_capacity = 3
-    dates_capacity = 1000
-    
-    
+    # aloca apresentações nas sessões
     for session in sessions_struct
         nPresentation, = size(session.presentations)
         finished = false
         if (nPresentation >= session.capacity)
             finished = true
         end
-        while(!finished)
+        while (!finished)
             best_presentation = nothing
             best_similarity = -1
             added = false
@@ -116,7 +131,6 @@ function greedy_heuristic(n_themes, n_authors, n_presentations, n_sessions, pres
                 end
             end
             if (added)
-                println(best_presentation)
                 
                 new_presentation = Presentation(best_presentation.id, best_presentation.nThemes,
                 best_presentation.nAuthors, best_presentation.themes,
@@ -131,8 +145,13 @@ function greedy_heuristic(n_themes, n_authors, n_presentations, n_sessions, pres
         end
     end
 
+    # verifica se há apresentação sem sessão e tenta alocar em alguma sessão
+    feasible = true
     for presentation in presentations_struct
         if (presentation.session == -1)
+            feasible = false
+            # println(presentation.id)
+            # cria um array de sessões disponíveis
             sessions_available = []
             for session in sessions_struct
                 nPresentations, = size(session.presentations)
@@ -141,17 +160,22 @@ function greedy_heuristic(n_themes, n_authors, n_presentations, n_sessions, pres
                 end
             end
             presentation_added = false
+
+            # para cada apresentação de cada sessão
+            # verifica se essa apresentação pode ser alocada em uma sessão disponível
+            # se sim, essa apresentação é retirada da sessão atual e colocada na sessão disponível
+            # a apresentação que estava sem sessão é colocada no lugar se for possível
             for session in sessions_struct
                 for presentation_ in session.presentations
                     for session_ in sessions_available
                         if (is_possible_select(presentations_struct[presentation_], presentations_struct, sessions_struct[session_], sessions_struct, schedules_capacity, dates_capacity))
-                            filter!(e -> e ≠ presentation_,session.presentations)
+                            filter!(e -> e != presentation_, session.presentations)
                             
                             if (is_possible_select(presentation, presentations_struct, session, sessions_struct, schedules_capacity, dates_capacity))
                                 new_presentation = Presentation(presentation.id, presentation.nThemes,
                                                     presentation.nAuthors, presentation.themes,
                                                     presentation.authors, session.id)
-                                
+                                # println("entrou no if")
                                 presentations_struct[presentation.id] = new_presentation
                                 append!(session.presentations, new_presentation.id)
 
@@ -162,6 +186,7 @@ function greedy_heuristic(n_themes, n_authors, n_presentations, n_sessions, pres
                                 presentations_struct[presentation_] = new_presentation
                                 append!(sessions_struct[session_].presentations, new_presentation.id)
                                 presentation_added = true
+                                feasible = true
                                 break
                             else
                                 append!( session.presentations, presentation_)
@@ -176,19 +201,58 @@ function greedy_heuristic(n_themes, n_authors, n_presentations, n_sessions, pres
                     break
                 end
             end
+            # println("saiu")
         end
     end
 
-    println("AQUI")
-    for presentation in presentations_struct
-        println(presentation)
-    end
+    # for presentation in presentations_struct
+    #     println(presentation)
+    # end
 
-    return sessions_struct
+    # println(sessions_struct[1])
+    return feasible, sessions_struct 
     
 end
 
-function heuristic(n_themes, n_authors, n_presentations, n_sessions, presentations_struct, sessions_struct)
+# function copy_sessions_structs(sessions_struct)
+#     k, = size(sessions_struct)
+#     new_structs = Array{Session, k}
+#     for s in sessions_struct
+#         new_id = s.id
+#         new_capacity = s.capacity
+#         new_presentationTime = s.presentationTime
+#         new_schedule = s.schedule
+#         new_date = s.date
+#         new_presentations = copy(s.presentations)
+
+#         new_struct = Session(new_id, new_capacity, new_presentationTime, new_schedule, new_date, new_presentations)
+#         println(new_struct)
+#         new_structs[new_id] = new_struct
+#     end
+#     return new_structs
+# end
+
+
+# function copy_presentations_structs(presentations_struct)
+#     k, = size(presentations_struct)
+#     new_structs = Array{Presentation, k}
+
+#     for p in presentations_struct
+#         new_id = p.id
+#         new_nThemes = p.nThemes
+#         new_nAuthors = p.nAuthors
+#         new_themes = copy(p.themes)
+#         new_authors = copy(p.authors)
+#         new_session = p.session
+
+#         new_struct = Presentation(new_id, new_nThemes, new_nAuthors, new_themes, new_authors, new_session)
+#         new_structs[new_id] = new_structs
+#     end
+#     return new_structs
+# end
+
+
+function heuristic(n_themes, n_authors, n_presentations, n_sessions, presentations_struct, sessions_struct, schedule_capacity, date_capacity, n_iteration)
     
     presentations = collect(1:n_presentations)
     sessions = collect(1:n_sessions)
@@ -196,22 +260,57 @@ function heuristic(n_themes, n_authors, n_presentations, n_sessions, presentatio
     # Calcula os valores de similaridade para cada par de apresentacoes
     presentations_similarity = calculate_similarity(n_presentations, presentations, presentations_struct)
     
-    t = @elapsed begin
-        results_struct = greedy_heuristic(n_themes, n_authors, n_presentations, n_sessions, presentations_struct, sessions_struct, presentations_similarity)
+    number_of_times = n_iteration
+    percentage_of_errors = 0
+    best_function_value = -1
+    best_session_struct = []
+    time_heuristic = @elapsed begin
+        for i in collect(1:number_of_times)
+            par_sessions_struct = deepcopy(sessions_struct)
+            par_presentations_struct = deepcopy(presentations_struct)
+            
+            # println(par_presentations_struct[1], " ", sessions_struct[1])
+            # println(par_sessions_struct[1], " ", sessions_struct[1])
+
+            feasible, results_struct = greedy_heuristic(n_themes, n_authors, n_presentations, n_sessions, par_presentations_struct, par_sessions_struct, presentations_similarity, schedule_capacity, date_capacity)
+            
+            function_value = 0
+    
+            for session in results_struct
+                for presentation1 in session.presentations
+                    for presentation2 in session.presentations
+                        if (presentation2 > presentation1)
+                            function_value = (
+                                function_value + 
+                                presentations_similarity[presentation1, presentation2]
+                            )
+                        end
+                    end
+                end
+            end
+
+            if (!feasible)
+                percentage_of_errors = percentage_of_errors + 1
+            elseif (function_value > best_function_value)
+                best_session_struct = deepcopy(results_struct)
+                best_function_value = function_value
+            end
+        end
+        percentage_of_errors = (percentage_of_errors / number_of_times) * 100
     end
-    println(t)
-    
-    
+    # println(results_struct[1])
+    # print("tempo de processamento: ")
+    # println(t)
     
     
     # for session in sessions_struct
-        # println("Sessão: ", session.id)
-        # println("horario: ", session.schedule)
-        # println("n presentation", session.presentations)
+    #     println("Sessão: ", session.id)
+    #     println("horario: ", session.schedule)
+    #     println("n presentation", session.presentations)
     # end
     
     presentation_sessions = zeros(n_presentations, n_sessions)
-    for session in sessions_struct
+    for session in best_session_struct
         for presentation in session.presentations
             # println(presentation, " ", n_presentations)
             presentation_sessions[presentation, session.id] = 1 
@@ -219,7 +318,9 @@ function heuristic(n_themes, n_authors, n_presentations, n_sessions, presentatio
     end
     
     output = ""
-    output = string("time : ", t, "\n")
+    # output = string(output, "schedule_capacity : ", schedule_capacity, "\n")
+    # output = string(output, "date_capacity : ", date_capacity, "\n")
+    output = string(output, "time : ", time_heuristic, "\n")
     for presentation in presentations
         for session in sessions
             output = string(
@@ -230,30 +331,11 @@ function heuristic(n_themes, n_authors, n_presentations, n_sessions, presentatio
         end
         output = string(output, "\n") 
     end
-    function_value = 0
     
-    for session in results_struct
-        println(session)
-        for presentation1 in session.presentations
-            for presentation2 in session.presentations
-                if (presentation2 > presentation1)
-                    function_value = (
-                        function_value + 
-                        presentations_similarity[presentation1, presentation2]
-                    )
-                end
-            end
-        end
-    end
 
         
     
-    output = string(output, "objective_function_value : ", function_value)
-    # println(output)
-    
-    
-    open("./results/temp.txt", "w") do f 
-        write(f, output)
-    end
-    
+    output = string(output, "objective_function_value : ", best_function_value, "\n")
+
+    return  best_function_value, output, time_heuristic, percentage_of_errors 
 end
